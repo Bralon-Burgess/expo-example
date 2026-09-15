c98a3e8b54fea40f5600c4f990fca267835039ce
Bralon-Burgess <bburgess40@gatech.edu>
2026-09-15 18:37:48 -0400

    Added firebase

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,16 +1,35 @@
 import { useSession } from "@/context/Authentication";
 import { Pressable, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar"
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
 
 export default function Authentication() {
 
     const { signIn } = useSession()
+
+    const sendData = async () => {
+        try {
+            await addDoc(collection(db, "test_messages"), {
+                message: "Hello from my iPhone!",
+                timestamp: new Date()
+            });
+            alert("Success! Data sent to Firebase.");
+        } catch (e) {
+            console.error("Error adding document: ", e);
+            alert("Failed to send data.");
+        }
+    };
 
     return (
         <View className="h-full justify-center items-center relative">
             <StatusBar style="auto" />
 
             <Text className="text-4xl font-extrabold text-gray-900 dark:text-gray-100">Welcome</Text>
+
+            <Pressable className="bg-green-500 p-4 mt-5 rounded-full" onPress={sendData}>
+                <Text className="text-white font-extrabold">Send Test Data</Text>
+            </Pressable>
 
             <Pressable className="absolute bottom-10 bg-[#2e78b7] p-4 rounded-full" onPress={() => signIn()}>
                 <Text className="text-white font-extrabold">Continue</Text>
---
+++ firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD0EX6u4qKNBdSF9TMvrc1yHCPqTdjChT4",
+  authDomain: "expo-test-29ccc.firebaseapp.com",
+  projectId: "expo-test-29ccc",
+  storageBucket: "expo-test-29ccc.firebasestorage.app",
+  messagingSenderId: "734472126239",
+  appId: "1:734472126239:web:eba4903a70f42fca1c3a2a",
+  measurementId: "G-FPQLMLKK4T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
